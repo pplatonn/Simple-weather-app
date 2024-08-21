90c02c4f5a098fbf7d36d8a393c04624ba2cd786
Polina Platonova <polinaplatonova122@gmail.com>
2024-08-21 17:33:11 +0300

    First commit of app
    Installing the API and desktop design. Adaptive will be pushed soon

---
+++ assets/js/main.js
@@ -0,0 +1,49 @@
+'use strict'
+
+// getting all the text fields from HTMl file 
+const weatherContainerTemp = document.querySelector('.weather_container_temp')
+const weatherContainerLocation = document.querySelector('.weather_container_location')
+const weatherContainerDate = document.querySelector('.weather_container_date')
+const weatherContainerCondition = document.querySelector('.weather_container_condition')
+
+// form fields 
+const searchField = document.querySelector('.weather_navbar > form > input')
+const searchBtn = document.querySelector('.weather_navbar > form > button')
+
+// http://api.weatherapi.com/v1/current.json?key=077e43c2c9214d62948124009242108&q=Vladimir&aqi=no
+
+// just my city for the default property 
+let targetLocation = 'Vladimir';
+
+// async function that uses free weather API and gets such parametres as city, time, date and temperature 
+const getResults = async (location) => {
+    let url = `http://api.weatherapi.com/v1/current.json?key=077e43c2c9214d62948124009242108&q=${targetLocation}&aqi=no`;
+    const response = await fetch(url)
+    const data = await response.json()
+    let locationName = data.location.name;
+    let temp = data.current.temp_c;
+    let time = data.location.localtime;
+    let condition = data.current.condition.text
+
+    // call function below to update information after clicking the button 
+    updateDetails(temp, locationName, time, condition)
+}
+
+// function which change the HTML fields to API properties from above 
+function updateDetails (temp, locationName, time, condition) {
+    weatherContainerTemp.textContent = temp;
+    weatherContainerLocation.textContent = locationName;
+    weatherContainerDate.textContent = time;
+    weatherContainerCondition.textContent = condition
+}
+
+// search the information from input tag value
+async function searchForLocation(element) {
+    element.preventDefault()
+    targetLocation = searchField.value
+    getResults(targetLocation)
+}
+
+searchBtn.addEventListener('click', searchForLocation)
+
+getResults(targetLocation)
